6c8dd8e8091d90b0544174b1eb8fe607319e16fa
Michael Zhu <mzhu23@gmail.com>
2020-06-08 12:21:51 +0000

    Completed challenge

---
+++ HackerRank/WarmUp/diagonalDifference/disgonal-difference.js
@@ -0,0 +1,9 @@
+function diagonalDifference(arr) {
+  let diagonal1 = 0;
+  let diagonal2 = 0;
+  for (let i = 0; i < arr.length; i++) {
+    diagonal1 += arr[i][i];
+    diagonal2 += arr[i][arr.length - 1 - i]
+  }
+  return Math.abs(diagonal1 - diagonal2);
+}
