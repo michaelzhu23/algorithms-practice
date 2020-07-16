a8dc36c07d70dc1657e4dc9630edfe59cb52fae2
Michael Zhu <mzhu23@gmail.com>
2020-07-16 08:54:07 +0000

    Added alternate solution to matrixElementsSum problem from CodeSignal

--- code-signal/problem_8-matrixElementsSum/matrix-elements-sum.js
+++ code-signal/problem_8-matrixElementsSum/matrix-elements-sum.js
@@ -40,3 +40,17 @@ function matrixElementsSum(matrix) {
   }
   return sum;
 }
+
+function matrixElementsSum2(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] === 0) {
+        break;
+      } else {
+        sum += matrix[j][i];
+      }
+    }
+  }
+  return sum;
+}
