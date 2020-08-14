cef77ba22523d3f4ce40975459c4d6f6d3354d89
Michael Zhu <mzhu23@gmail.com>
2020-08-14 08:46:07 +0000

    Added alternate solution to addTwoDigits problem from CodeSignal

--- code-signal/the-core-challenges/problem_1-addTwoDigits/add-two-digits.js
+++ code-signal/the-core-challenges/problem_1-addTwoDigits/add-two-digits.js
@@ -6,3 +6,7 @@ function addTwoDigits(n) {
   const nToString = n + "";
   return parseInt(nToString[0]) + parseInt(nToString[1]);
 }
+
+function addTwoDigits2(n) {
+  return Math.floor(n / 10) + n % 10;
+}
