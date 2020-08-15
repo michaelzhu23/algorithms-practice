194e75c19c35183906226a120c0c4bc6d4e4665f
Michael Zhu <mzhu23@gmail.com>
2020-08-15 10:55:33 +0000

    Added solution to largestNumber problem from CodeSignal

---
+++ code-signal/the-core-challenges/problem_2-largestNumber/largest-number.js
@@ -0,0 +1,8 @@
+function largestNumber(n) {
+  // set number 9 to a variable
+  // convert variable to string
+  // repeat string n times
+  // convert back to number
+  const number = 9
+  return parseInt(number.toString().repeat(n));
+}
