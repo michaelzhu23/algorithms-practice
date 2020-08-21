14cfe3e43f0b81b74e4de6996511f8d6ba3c5500
Michael Zhu <mzhu23@gmail.com>
2020-08-21 00:31:18 -0700

    Added pseudo code to solution of adjacentElementsProduct problem from CodeSignal

--- code-signal/intro-challenges/problem_4-adjacentElementsProduct/adjacent-elements-product.js
+++ code-signal/intro-challenges/problem_4-adjacentElementsProduct/adjacent-elements-product.js
@@ -1,6 +1,10 @@
 function adjacentElementsProduct(inputArray) {
+  // declare variable with value assigned to product of the first 2 array elements
+  // loop thru array starting from index 1 and multiple by next index
+  // if product at current loop index > value of variable outside of loop, reassign value of variable to current product in loop
+  // return variable after the loop, which should have the value of the largest product
   let largestProduct = inputArray[0] * inputArray[1]
-  for (let i = 0; i < inputArray.length; i++) {
+  for (let i = 1; i < inputArray.length; i++) {
     const currentAdjacentProduct = inputArray[i] * inputArray[i + 1];
     if (currentAdjacentProduct > largestProduct) {
       largestProduct = currentAdjacentProduct;
