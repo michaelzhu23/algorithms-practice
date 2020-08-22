c67f948af770da3ffd1321c0495949b270ec3df1
Michael Zhu <mzhu23@gmail.com>
2020-08-22 02:53:21 -0700

    Added pseudo code to solution of commonCharacterCount problem from CodeSignal

--- code-signal/intro-challenges/problem_10-commonCharacterCount/common-character-count.js
+++ code-signal/intro-challenges/problem_10-commonCharacterCount/common-character-count.js
@@ -1,4 +1,9 @@
 function commonCharacterCount(s1, s2) {
+  // create counter variable to count common characters that starts at 0
+  // split and sort both input strings into sorted array
+  // use nested for loop to compare each element of one array with all elements of the other array
+  // if characters are equal, increment counter variable by 1 and remove the element from the index of the array from the outer for loop so that the next element can be compared without duplicating comparisons
+  // return counter array
   let counter = 0;
   const s1Array = s1.split("").sort();
   const s2Array = s2.split("").sort();
