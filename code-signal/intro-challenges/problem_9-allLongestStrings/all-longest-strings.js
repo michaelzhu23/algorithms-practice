c5f775ae509ab7e4e5343da77ea574ad7133f782
Michael Zhu <mzhu23@gmail.com>
2020-08-22 02:23:51 -0700

    Added pseudo code to solution of allLongestStrings problem from CodeSignal

--- code-signal/intro-challenges/problem_9-allLongestStrings/all-longest-strings.js
+++ code-signal/intro-challenges/problem_9-allLongestStrings/all-longest-strings.js
@@ -1,4 +1,9 @@
 function allLongestStrings(inputArray) {
+  // create a variable that stores length of longest string set to an initial value of 0
+  // create new empty array to store all longest strings
+  // loop thru input array and compare string length of current index to created variable and if greater than, set to current index's string's length
+  // loop thru input array a 2nd time and push elements into empty array if string length equals longest string length
+  // return new array with all longest strings
   let longestString = 0;
   const newArray = [];
   for (let i = 0; i < inputArray.length; i++) {
