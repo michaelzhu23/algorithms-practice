419e46fa24f6fe076c56335dae242d79b106aa45
Michael Zhu <mzhu23@gmail.com>
2020-08-24 16:34:31 -0700

    Added pseudo code to solution of highestDigit problem from Edabit

--- edabit/highestDigit/highest-digit.js
+++ edabit/highestDigit/highest-digit.js
@@ -1,4 +1,8 @@
 function highestDigit(number) {
+  // convert number to string in order to iterate thru each digit
+  // create variable to compare and store highest digit
+  // loop thru string to compare with variable and set variable to current string digit to variable if higher
+  // convert highest digit variable back to number and return
   const string = number + "";
   let highest = 0;
   for (let i = 0; i < string.length; i++) {
