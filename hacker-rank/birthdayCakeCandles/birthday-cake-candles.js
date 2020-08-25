a2c730f013a99c2c195a01be668d88198ae40616
Michael Zhu <mzhu23@gmail.com>
2020-08-25 15:26:21 -0700

    Added pseudo code to solution of birthdayCakeCandles problem from HackerRank

--- hacker-rank/birthdayCakeCandles/birthday-cake-candles.js
+++ hacker-rank/birthdayCakeCandles/birthday-cake-candles.js
@@ -1,4 +1,8 @@
 function birthdayCakeCandles(ar) {
+  // find highest number in input array with Math.max() using the spread operator
+  // declare a variable to count total number of highest number
+  // loop thru array and if current index = highest number, increment count by 1
+  // return count variable
   let highestCandle = Math.max(...ar);
   let count = 0;
   for (let i = 0; i < ar.length; i++) {
