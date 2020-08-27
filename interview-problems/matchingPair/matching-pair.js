ed49cc3d13c08bbb2cec3e9951f9057b714fcefe
Michael Zhu <mzhu23@gmail.com>
2020-08-27 04:05:59 -0700

    Added pseudo code to O(n) solution of matchingPair problem from Cherie's interview with Ultra Mobile

--- interview-problems/matchingPair/matching-pair.js
+++ interview-problems/matchingPair/matching-pair.js
@@ -12,6 +12,12 @@ function matchingPair(arr, num) {
 }
 
 function matchingPair2(arr, num) {
+  // create empty array to store matching pair
+  // create empty object to store each array element as properties with value of true
+  // loop thru array to add property of each element to object
+  // loop thru array again to check for number corresponding to current array index that equals to the 2nd argument within object
+  // if corresponding number exists, push both numbers into empty array
+  // return array
   const matchArray = []
   const object = {}
   for (let i = 0; i < arr.length; i++) {
