3b024733c040e6a9ffb0bfa325e81f97319876f4
Michael Zhu <mzhu23@gmail.com>
2021-09-15 14:16:09 -0700

    Added technical challenge problem from EAB

---
+++ interview-problems/filterDataWithFormInputValues/index.js
@@ -0,0 +1,15 @@
+const input = [
+  { id: 101, course: "English", comments: "" },
+  { id: 102, course: "Math", comments: "This is a math comment" },
+  { id: 103, course: "History", comments: "This is a history comment" },
+];
+
+const formData = {
+  101: "This is a new english comment",
+  102: "This is a math comment",
+  103: "This is a history comment",
+};
+
+function postNewComments(input, formData) {
+  // return array of objects with new comments
+}
