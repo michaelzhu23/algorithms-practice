c23e881e0ed002dbf46bff19f103b5c5b5d9ef51
Michael Zhu <mzhu23@gmail.com>
2021-09-19 00:22:45 -0700

    Updated challenge with 2 solutions

--- interview-problems/filterDataWithFormInputValues/index.js
+++ interview-problems/filterDataWithFormInputValues/index.js
@@ -1,4 +1,4 @@
-const input = [
+const currentData = [
   { id: 101, course: "English", comments: "" },
   { id: 102, course: "Math", comments: "This is a math comment" },
   { id: 103, course: "History", comments: "This is a history comment" },
@@ -10,6 +10,55 @@ const formData = {
   103: "This is a history comment",
 };
 
+// create new empty array to contain updated data
+// loop thru one data set and compare comments of both
+// if comments are not equal, set comment to new comment
+// push object with new comment into new array to be returned
+// return array of only objects that has new comments
+
 function postNewComments(input, formData) {
   // return array of only objects that has new comments and with all other properties
+  const updatedData = [];
+
+  for (const id in formData) {
+    const matchingData = input.find((element) => {
+      return element.id == id;
+    });
+
+    if (matchingData.comments !== formData[id]) {
+      matchingData.comments = formData[id];
+      updatedData.push(matchingData);
+    }
+  }
+
+  return updatedData;
 }
+
+console.log("postNewComments:", postNewComments(currentData, formData));
+
+const currentData2 = [
+  { id: 101, course: "English", comments: "This is a english comment" },
+  { id: 102, course: "Math", comments: "" },
+  { id: 103, course: "History", comments: "This is a history comment" },
+];
+
+const formData2 = {
+  101: "This is a english comment",
+  102: "This is a new math comment",
+  103: "This is a history comment",
+};
+
+function postNewComments2(input, formData) {
+  const updatedData = [];
+
+  input.forEach((element) => {
+    if (element.comments !== formData[element.id]) {
+      element.comments = formData[element.id];
+      updatedData.push(element);
+    }
+  });
+
+  return updatedData;
+}
+
+console.log("postNewComments2:", postNewComments2(currentData2, formData2));
